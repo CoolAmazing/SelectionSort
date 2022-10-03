8e115427f1d922f488610b481622127cfc2cb311
Andrew Liu <andrew.liu@student.allenisd.org>
2022-10-03 13:08:26 -0500

    first commit

---
+++ main.swift
@@ -0,0 +1,30 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [5, 1, 4, 2, 8]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+func selectionSort(_ array: [Int]) {
+
+    var a = array  
+    print("Pass: \(0), Swaps: \(0)/\(0), Array: \(a)")
+    for x in 0 ..< a.count - 1 {
+
+        var lowest = x
+        for y in x + 1 ..< a.count {
+            if a[y] < a[lowest] {
+                lowest = y
+            }
+        }
+        if x != lowest {            
+            a.swapAt(x, lowest)
+        }
+        print("Pass: \(x+1), Swaps: \(1)/\(x+1), Array: \(a)")
+    }
+}
+selectionSort(unsortedIntegers)
+
